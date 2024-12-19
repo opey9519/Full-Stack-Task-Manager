d7f48c419639f86c19446c63559d54c2a3aacb27
Gavin Wilson <gmanwilson@icloud.com>
2024-12-19 12:32:28 -0500

    Added modify_task function to take in id and modify task name/completion

--- task.py
+++ task.py
@@ -10,18 +10,31 @@ Complete: Contains boolean value
 '''
 tasks = []
 
-# Route to get all tasks
+# Route to get all tasks using GET method
 @app.route("/tasks", methods = ["GET"])
 def get_tasks():
     return jsonify(tasks), 200
 
-# Add task to task manager
+# Add task to task manager using POST method
 @app.route("/tasks", methods = ["POST"])
 def add_task():
-    data =request.get_json()
+    data = request.get_json() # parses requested data and stores in Python dictionary
+    # With the data object, .get() to find name of task and implement it into dictionary
     new_task = {
         "name": data.get("name"),
         "id": len(tasks) + 1,
         "complete": False
     }
+    tasks.append(new_task) # Append new task to Virtual database
     return jsonify(new_task), 201
+
+# Modify task in task manager given task id using PUT method
+@app.route("/tasks/int:<id>", methods = {"PUT"})
+def modify_task(id):
+    data = request.get_json()
+    for task in tasks:
+        if tasks["id"] == id:
+            task["name"] = data.get("name", task["name"])
+            task["completed"] + data.get("completed", task["completed"])
+            return jsonify(task), 200
+    return print("Error: Task not found"), 404
